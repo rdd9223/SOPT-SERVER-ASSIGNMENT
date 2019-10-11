0884d253c558b9534a78ad35cb3b8264266d5cdf
윤희성 <heesung6701@naver.com>
2019-10-11 19:13:48 +0900

    model 추가

---
+++ assignment2/model/group.js
@@ -0,0 +1,7 @@
+class Group {
+    constructor(idx, name) {
+        this.idx = idx;
+        this.name = name;
+    }
+}
+module.exports = Group;
---
+++ assignment2/model/member.js
@@ -0,0 +1,7 @@
+class Member {
+    constructor(name, groupIdx) {
+        this.name = name;
+        this.groupIdx = groupIdx;
+    }
+}
+module.exports = Member;
