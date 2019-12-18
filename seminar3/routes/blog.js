273050d6ba5160b0fdc7244ba0137e265a43ca22
YeongWoooo <46023074+rdd9223@users.noreply.github.com>
2019-12-19 00:35:05 +0900

    add put

--- seminar3/routes/blog.js
+++ seminar3/routes/blog.js
@@ -9,13 +9,13 @@ const db = require('../module/pool');
 router.get('/', async(req, res) => {
     const table = 'blog';
 
-    const getAllBlogsQuery = `SELECT * FROM ${table}`
-    const getAllBlogsResult = db.queryParam_Parse(getAllBlogsQuery)
+    const getAllBlogsQuery = `SELECT * FROM ${table}`;
+    const getAllBlogsResult = db.queryParam_Parse(getAllBlogsQuery);
 
     if(!getAllBlogsResult){
-        res.status(statusCode.BAD_REQUEST).send(authUtil.successFalse(responseMessage.INTERNAL_SERVER_ERROR))
+        res.status(statusCode.BAD_REQUEST).send(authUtil.successFalse(responseMessage.INTERNAL_SERVER_ERROR));
     } else {
-        res.status(statusCode.OK).send(authUtil.successTrue(responseMessage.BOARD_READ_SUCCESS, getAllBlogsResult))
+        res.status(statusCode.OK).send(authUtil.successTrue(responseMessage.BOARD_READ_SUCCESS, getAllBlogsResult));
     }
 })
 
@@ -29,9 +29,25 @@ router.post('/', async(req, res) => {
     const postBlogQuery = `INSERT INTO blog (title, writer, intro) VALUES ('${title}', '${writer}', '${intro}')`;
     const postBlogResult = await db.queryParam_Parse(postBlogQuery);
 
-    if(!result) {
+    if(!postBlogResult) {
         res.status(500).send('error');
     }
-    res.status(statusCode.CREATED).send(authUtil.successTrue(responseMessage.BOARD_CREATE_SUCCESS, postBlogResult))
+    res.status(statusCode.CREATED).send(authUtil.successTrue(responseMessage.BOARD_CREATE_SUCCESS, postBlogResult));
+})
+
+router.put('/', async(req, res) => {
+    const {title, writer, intro} = req.body;
+    
+    if (!title || !writer || !intro) {
+        res.status(statusCode.BAD_REQUEST).send(authUtil.successFalse(responseMessage.NULL_VALUE));
+    }
+
+    const putBlogQuery = `UPDATE blog SET title = ${title}, intro = ${intro} WHERE writer = ${writer}`;
+    const putBlogResult = await db.queryParam_Parse(putBlogQuery);
+
+    if(!putBlogResult) {
+        res.status(500).send('error');
+    }
+    res.status(statusCode.CREATED).send(authUtil.successTrue(responseMessage.BOARD_CREATE_SUCCESS, putBlogResult));
 })
 module.exports = router;
