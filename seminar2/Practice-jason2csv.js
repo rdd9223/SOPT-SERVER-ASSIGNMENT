12549889c8c802cf23c8b79bd6f255d6ac2f34c4
YeongWoooo <46023074+rdd9223@users.noreply.github.com>
2019-12-18 23:39:42 +0900

    move directory

--- seminar2/Practice-jason2csv.js
+++
@@ -1,7 +0,0 @@
-const jason2csv = require('json2csv');
-const jsonArray = [{id:'heesung', pw: 'admin', name: '관리자'},
-{id:'heesung', pw: '1q2w3e4r!', name: '윤희성'},
-{id: 'starbucks', pw: 'JamongBlackHoneyTea', name: '스타벅스'}];
-
-const resultCsv = json2csv.parse(jsonArray)
-console.log(resultCsv)
